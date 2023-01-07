cf4aec2017982337cc56bac256268f3d865bc859
sjh9391985 <sjh9391985@gmail.com>
2023-01-07 21:02:44 +0900

    java version issue solve (8 -> 11)

---
+++ src/main/java/jpa/basic/Locker.java
@@ -0,0 +1,14 @@
+package jpa.basic;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Locker {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+}
