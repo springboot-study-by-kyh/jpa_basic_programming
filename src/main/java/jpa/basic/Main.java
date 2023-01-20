53fa0a79796242dcb6991885240ddd77a6ea0500
sjh9391985 <sjh9391985@gmail.com>
2023-01-20 10:48:33 +0900

    JPQL 쿼리

--- src/main/java/jpa/basic/Main.java
+++ src/main/java/jpa/basic/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        EntityManagerFactory entityManagerFactory =  Persistence.createEntityManagerFactory("hello");
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
 
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
@@ -22,34 +22,11 @@ public class Main {
         entityTransaction.begin();
 
         try {
+            List<Member> result = entityManager.createQuery(
+                "select m From Member m where m.username like 'kim%'"
+                , Member.class
+            ).getResultList();
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeAddress(new Address("city1", "street1", "zipcode1"));
-
-            member.getFavoriteFoods().add("chicken");
-            member.getFavoriteFoods().add("pizza");
-            member.getFavoriteFoods().add("coke");
-
-            member.getAddressHistory().add(new AddressEntity("old1", "street1", "zipcode1"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street1", "zipcode1"));
-
-            entityManager.persist(member);
-
-            entityManager.flush();
-            entityManager.clear();
-
-            Member findMember = entityManager.find(Member.class, member.getId());
-
-            Address a  = findMember.getHomeAddress();
-
-            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
-
-            findMember.getFavoriteFoods().remove("chicken");
-            findMember.getFavoriteFoods().add("korean foods");
-
-            findMember.getAddressHistory().remove(new AddressEntity("old1", "street1", "zipcode1"));
-            findMember.getAddressHistory().add(new AddressEntity("newCity1", "street1", "zipcode1"));
 
             entityTransaction.commit();
         } catch (Exception e) {
