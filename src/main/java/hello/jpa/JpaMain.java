c780c5988a94bc79c9cb5b4da244d09e59ab292a
sjh9391985 <sjh9391985@gmail.com>
2023-02-06 17:30:10 +0900

    JPA 장점: 쿼리를 직접 작성필요X

--- src/main/java/hello/jpa/JpaMain.java
+++ src/main/java/hello/jpa/JpaMain.java
@@ -1,9 +1,31 @@
 package hello.jpa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class JpaMain {
 
     public static void main(String[] args) {
-        
+        // EntityManagerFactory: 로딩시점에 하나만 만들어놔야함.
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setName("hello");
+        em.persist(member);  // 저장
+
+        tx.commit();
+
+        // code
+        em.close();
+
+        emf.close();
     }
 
 }
